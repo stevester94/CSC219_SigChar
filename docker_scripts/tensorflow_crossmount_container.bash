52fa82854297bd7d55fc9db01888f7166d4cf744
Steven Mackey <stevester94@gmail.com>
2019-04-10 20:46:53 -0700

    Multilayer now working

--- docker_scripts/tensorflow_crossmount_container.bash
+++ docker_scripts/tensorflow_crossmount_container.bash
@@ -1,2 +1,2 @@
-docker run -it -u $(id -u):$(id -g) -v $(realpath ..):/cross_mount  --rm --runtime=nvidia tensorflow/tensorflow:latest-gpu-py3 bash
+docker run -it --name="Hephaestus" -u $(id -u):$(id -g) -v $(realpath ..):/cross_mount  --rm --runtime=nvidia tensorflow/tensorflow:latest-gpu-py3 bash
 
--- working_set/multilayer_perceptron/multilayer.py
+++ working_set/multilayer_perceptron/multilayer.py
@@ -3,6 +3,7 @@
 import tensorflow as tf
 import deepsig_accessor as ds_accessor
 import random
+from time import sleep
 
 # Seed our randomizer for reproducability
 random.seed(1337)
@@ -29,27 +30,32 @@ for i in range(0, set_split_point):
 
 for i in range(set_split_point, len(dataset)):
     test_x.append(dataset[i][0])
-    test_x.append(dataset[i][2])
+    test_y.append(dataset[i][2])
 
 # Python optimisation variables
-learning_rate = 0.5
-epochs = 10000
-NUM_HIDDEN_NODES = 10
+learning_rate = 0.001 # Orignally 0.5
+epochs = 300
+
+# Notes on identity function:
+# 10 works well for 1 hidden, not for 3
+NUM_HIDDEN_NODES = 256
 
 
 # Code to hijack the shit and just train an identity function
-if True:
-    LEN_X = 10
-    LEN_Y = LEN_X
-    train_x = []
-    train_y = []
-    for i in range(LEN_X):
-        row = [0] * LEN_X
-        row[i] = 1
-        train_x.append(row)
-        train_y.append(row)
-    test_x = train_x
-    test_y = train_y
+# if False:
+#     print("Hijacking for identity testing!")
+#     sleep(5)
+#     LEN_X = 10
+#     LEN_Y = LEN_X
+#     train_x = []
+#     train_y = []
+#     for i in range(LEN_X):
+#         row = [0] * LEN_X
+#         row[i] = 1
+#         train_x.append(row)
+#         train_y.append(row)
+#     test_x = train_x
+#     test_y = train_y
 
 # declare the training data placeholders
 # Input 10 nodes, output 10 nodes
@@ -78,54 +84,50 @@ b4 = tf.Variable(tf.random_normal([LEN_Y]), name='b4')
 #  Hidden layer: Multiply input by the weights, add bias, rectify
 hidden_out = tf.nn.relu(tf.add(tf.matmul(x, W1), b1))
 hidden_out = tf.nn.relu(tf.add(tf.matmul(hidden_out, W2), b2))
-hidden_out = tf.nn.relu(tf.add(tf.matmul(hidden_out, W3), b3))
+# hidden_out = tf.nn.relu(tf.add(tf.matmul(hidden_out, W3), b3))
+out_layer = tf.add(tf.matmul(hidden_out, W4), b4)
 
 
-
-# now calculate the hidden layer output - in this case, let's use a softmax activated
-# output layer
-y_ = tf.nn.softmax(tf.add(tf.matmul(hidden_out, W4), b4))
-
-#converting the output y_ to a clipped version, limited between 1e-10 to 0.999999.  
-# This is to make sure that we never get a case were we have a log(0) operation occurring during training 
-# – this would return NaN and break the training process.
-y_clipped = tf.clip_by_value(y_, 1e-10, 0.9999999)
-
-
-cross_entropy = -tf.reduce_mean(tf.reduce_sum(y * tf.log(y_clipped)
-                                              + (1 - y) * tf.log(1 - y_clipped), axis=1))
+# No clue why, but the magic comes from this, was originally using some crazy ass logarithm and clipping
+loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
+    logits=out_layer, labels=y))
 
 # add an optimiser
-optimiser = tf.train.GradientDescentOptimizer(
-    learning_rate=learning_rate).minimize(cross_entropy)
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+
+train_op = optimizer.minimize(loss_op)
+
+
+
 
 
 # finally setup the initialisation operator
 init_op=tf.global_variables_initializer()
 
-# define an accuracy assessment operation
-correct_prediction=tf.equal(
-    tf.argmax(y, 1), tf.argmax(y_, 1))
-
-accuracy=tf.reduce_mean(
-    tf.cast(correct_prediction, tf.float32))
-
 # start the session
 with tf.Session() as sess:
-   # initialise the variables
-   sess.run(init_op)
+    # initialise the variables
+    sess.run(init_op)
 
-   # Not doing batches, do the whole training data each epoch
-   for epoch in range(epochs):
-    _, c = sess.run([optimiser, cross_entropy],
-                    feed_dict={x: train_x, y: train_y})
-    
-    print("Epoch:", (epoch + 1), "cost =", "{:.3f}".format(c))
+    # Not doing batches, do the whole training data each epoch
+    for epoch in range(epochs):
+        _, c = sess.run([train_op, loss_op],
+                feed_dict={x: train_x, y: train_y})
 
+        print("Epoch:", (epoch + 1), "cost =", "{:.3f}".format(c))
 
-   print("Accuracy on test set: %d" % sess.run(accuracy, feed_dict={
-         x: test_x, y: test_y}))
+    ##############
+    # Test model
+    ##############
 
-   print("Testing: " + str(train_x[0]))
-   print(sess.run(y_, feed_dict={
-       x: [train_x[0]] }))
+    # Softmax the output
+    pred = tf.nn.softmax(out_layer)
+
+    # For each categorical output, see if they're equal and return a tensor
+    correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
+
+    # Cast equality vector to float, take the mean of the whole thing
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+
+    # Run the shit on our test sets
+    print("Accuracy:", accuracy.eval(feed_dict={x: test_x, y: test_y}))
